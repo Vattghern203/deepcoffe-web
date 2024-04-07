23c8c6602512b7577502695474deb8d545f12586
Otávio Augusto <otavioaugustobrb19@gmail.com>
2024-04-07 12:00:43 -0300

    fix: icon importing error

--- src/components/organisms/UploadFiles/UploadFiles.tsx
+++ src/components/organisms/UploadFiles/UploadFiles.tsx
@@ -3,6 +3,7 @@
  * @see https://v0.dev/t/mjOlVnIqPys
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
+import { FileIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 
 export default function Component() {
@@ -15,7 +16,7 @@ export default function Component() {
                     <FileIcon className="w-6 h-6" />
                     <span className="font-bold">Drag and drop your files here</span>
                 </span>
-                <label className="text-center text-sm text-gray-500 dark:text-gray-400" htmlFor="file-form">
+                <label className="text-center text-sm text-gray-500 dark:text-gray-400 hover:cursor-pointer" htmlFor="file-form">
                     or
                     <Button className="ml-2 -z-10" size="sm" onClick={() => document.getElementById('file-form')?.click()}>Choose a file to upload
 
@@ -25,25 +26,5 @@ export default function Component() {
                 </label>
             </label>
         </section>
-    )
-}
-
-function FileIcon({ ...props }) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
-            <polyline points="14 2 14 8 20 8" />
-        </svg>
     )
 }
