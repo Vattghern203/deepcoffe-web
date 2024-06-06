d523e3a6ad44371b651edfe3860cf942accd3f20
Otávio Augusto <otavioaugustobrb19@gmail.com>
2024-06-06 13:45:57 -0300

    fix: rendering issues in randomGrid component

--- src/components/atoms/RandomGrid/RandomGrid.tsx
+++ src/components/atoms/RandomGrid/RandomGrid.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import { Suspense } from "react";
 
 import { Button } from "@/components/ui/button";
 
 import styles from "./random-grid.module.css";
+import { ImageSkeleton } from "@/layouts/GenericSkeletons";
 
 type GalleryBtnVariantType = {
   buttonLabel: "Analisar Imagem" | "Abrir Galeria";
@@ -10,26 +11,35 @@ type GalleryBtnVariantType = {
 };
 
 interface RandomGridProps {
-  children: ReactNode;
   uploadCount: number;
   isLoading: boolean;
+  imgArray: string[]
+  maxImageShow: number
   cols?: number;
   singleImageAction: () => void | Promise<void>;
   multImageAction: () => void | Promise<void>;
 }
 
-export default function RandomGrid({ children, uploadCount, cols = 3, isLoading, singleImageAction, multImageAction }: RandomGridProps) {
+export default function RandomGrid({
+  uploadCount,
+  isLoading,
+  imgArray,
+  maxImageShow = 9,
+  cols = 3,
+  singleImageAction,
+  multImageAction
+}: RandomGridProps) {
 
   const btnVariantCheck = uploadCount === 1 ? 1 : 0;
 
   const btnVariants: GalleryBtnVariantType[] = [
     {
       buttonLabel: "Abrir Galeria",
-      onClickFn: singleImageAction,
+      onClickFn: multImageAction,
     },
     {
       buttonLabel: "Analisar Imagem",
-      onClickFn: multImageAction,
+      onClickFn: singleImageAction
     },
   ];
 
@@ -37,7 +47,7 @@ export default function RandomGrid({ children, uploadCount, cols = 3, isLoading,
 
   return (
     <>
-      <section className="flex flex-col justify-center items-center mb-10 max-h-[600px]">
+      <section className="flex flex-col justify-center items-center mb-10 max-h-[600px] overflow-hidden">
         <div className={`
           max-h-[600px] relative overflow-clip after:w-full after:h-[50%] after:absolute after:left-[50%] after:translate-x-[-50%] after:bottom-0 after:bg-gradient-to-t after:gradient after:from-card after:to-transparent group ${styles.content_wrapper}`}>
           <h2 className="text-2xl font-bold mb-2 text-start self-start align-middle flex items-end">
@@ -49,7 +59,17 @@ export default function RandomGrid({ children, uploadCount, cols = 3, isLoading,
               className={`${styles.random_grid} py-6 px-4 w-full max-w-3xl overflow-y-auto mx-auto gap-1 rounded-md bg-card border`}
               style={{ columnCount: cols }}
             >
-              {children}
+              {imgArray.map((elem, idx) => idx < maxImageShow && (
+                <Suspense key={elem} fallback={<ImageSkeleton />}>
+                  <img
+                    loading="lazy"
+                    title={elem}
+                    className="rounded-md w-full block object-cover"
+                    src={elem}
+                    alt="Sample Image"
+                  />
+                </Suspense>
+              ))}
             </section>
           </div>
 
--- src/components/templates/UploadFiles/UploadFiles.tsx
+++ src/components/templates/UploadFiles/UploadFiles.tsx
@@ -1,149 +1,103 @@
-import { Suspense, useState, useEffect } from "react"
-import { Loader } from "lucide-react"
+import { useState, useEffect } from "react";
+import { Loader } from "lucide-react";
 
-import { useImage } from "@/hooks/useImageContext"
+import serverRepository from "@/common/repository/ServerRepository";
 
-import { Dropzone, UploadFilesConfirmation } from "@/components/organisms"
-import { ImageSkeleton } from "@/layouts/GenericSkeletons"
-import { RandomGrid } from "@/components/atoms"
-import { Result, SampleGallery } from "@/components/molecules"
+import { useImage } from "@/hooks/useImageContext";
 
-import { SelectedImage } from "@/types/ImageTypes"
-import serverRepository from "@/common/repository/ServerRepository"
+import { RandomGrid } from "@/components/atoms";
+import { Result, SampleGallery } from "@/components/molecules";
+import { Dropzone, UploadFilesConfirmation } from "@/components/organisms";
+
+import { SelectedImage } from "@/types/ImageTypes";
 
 type IResultData = {
-  label: string
-  value: number
-}
+  label: string;
+  value: number;
+};
 
 export default function UploadFiles() {
+  const imageContext = useImage();
 
-  const imageContext = useImage()
+  const [fileStack, setFileStack] = useState<File[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showGallery, setShowGallery] = useState(false);
+  const [data, setData] = useState<IResultData[]>([]);
+  const [isBeingDragged, setIsBeingDragged] = useState(false);
 
-  const [fileStack, setFileStack] = useState<File[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [showGallery, setShowGallery] = useState(false)
-  const [data, setData] = useState<IResultData[]>([])
-  const [isBeingDragged, setIsBeingDragged] = useState(false)
-
-  let imageData: SelectedImage[] = []
+  let imageData: SelectedImage[] = [];
 
   const handleUpload = async () => {
+    console.log("UPLOAD CLICKED");
 
-    console.log('UPLOAD CLICKED')
-
-    /* if (fileStack.length === 0) return */
-
-    if (fileStack.length === 1) {
-
-      imageContext.setSelectedImage(imageData[0])
-    }
-
-    //imageContext.setSelectedImage(imageData[0])
-
-    renewUpload()
-    setIsLoading(true)
+    setIsLoading(true);
 
     try {
-
-      const raw64 = await imageContext.selectedImage?.base64
+      const raw64 = await imageContext.selectedImage?.base64;
 
       if (raw64) {
+        const image = raw64.replace(/^data:image\/\w+;base64,/, "");
 
-        const image = raw64.replace(/^data:image\/\w+;base64,/, '')
-
-        const res = await serverRepository.post<{
-          cerscospora: number;
-          healthy: number;
-          leafRust: number;
-          miner: number;
-          phoma: number;
-      }, {image: string}>('/classify', {
-          image
-        },
-        false,
-        120000
-        )
+        const res = await serverRepository.post<
+          { cerscospora: number; healthy: number; leafRust: number; miner: number; phoma: number },
+          { image: string }
+        >("/classify", { image }, false, 120000);
 
         const typedRes = Object.entries(res.data).map(([key, value]) => ({
           label: key,
-          value
-        }))
+          value,
+        }));
 
-        setData(typedRes)
+        setData(typedRes);
+        renewUpload()
 
-        console.log(typedRes)
+        console.log(typedRes);
       }
-
     } catch (error) {
 
-      console.error(error)
-
+      console.error(error);
     } finally {
 
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
-  const createNamedBlob = (file: File) => URL.createObjectURL(file)
-
-  console.log("Named Blob created")
+  const createNamedBlob = (file: File) => URL.createObjectURL(file);
 
   const convertBlobToBase64 = async (blob: Blob): Promise<string> => {
-
-    const reader = new FileReader()
-    reader.readAsDataURL(blob)
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
 
     return new Promise<string>((resolve) => {
-
-      reader.onloadend = () => resolve(reader.result as string)
-    })
-  }
-
-  const namedBlobs = fileStack.map((file) => createNamedBlob(file))
-
-  const mockLoading = async (image: File): Promise<IResultData[]> => {
-
-    const base64 = await convertBlobToBase64(image)
-    console.log(base64)
-
-    return new Promise<IResultData[]>((resolve) =>
-      resolve([
-        { label: "doenca 01", value: 10 },
-        { label: "doenca 02", value: 90 },
-        { label: "doenca 03", value: 35.55 },
-      ])
-    )
-  }
+      reader.onloadend = () => resolve(reader.result as string);
+    });
+  };
 
   const renewUpload = () => {
-    setData([])
-  }
+    setData([]);
+  };
 
   const createImageDataByUpload = async () => {
 
     if (fileStack.length !== 0) {
-
       imageData = await Promise.all(
-
         fileStack.map(async (file) => ({
-
           path: createNamedBlob(file),
           base64: await convertBlobToBase64(file),
           file,
         }))
-      )
-
-      console.log(imageData)
+      );
+      console.log(imageData);
     } else {
-
-      imageData = []
+      imageData = [];
     }
-  }
+  };
 
   useEffect(() => {
-    createImageDataByUpload()
-  }, [fileStack])
+    createImageDataByUpload();
+  }, [fileStack]);
+
+  const namedBlobs = fileStack.map((file) => createNamedBlob(file));
 
   return (
     <>
@@ -171,32 +125,17 @@ export default function UploadFiles() {
         <RandomGrid
           uploadCount={namedBlobs.length}
           isLoading={isLoading}
-          singleImageAction={() => setShowGallery(true)}
-          multImageAction={handleUpload}
-        >
-          {namedBlobs.map((elem, idx) => (
-            idx < 9 && (
-              <Suspense key={elem} fallback={<ImageSkeleton />}>
-                <img
-                  loading="lazy"
-                  title={elem}
-                  className="rounded-md w-full block object-cover"
-                  src={elem}
-                  alt="Sample Image"
-                />
-              </Suspense>
-            )
-          ))}
-        </RandomGrid>
+          imgArray={namedBlobs}
+          maxImageShow={9}
+          singleImageAction={() => handleUpload()}
+          multImageAction={() => setShowGallery(true)}
+        />
       )}
 
       <SampleGallery.Root isOpen={showGallery} onOpenChangeFn={() => setShowGallery(false)} setIsOpen={setShowGallery}>
         <SampleGallery.Header galleryTitle="Uploads" galleryDescription="Selecione uma das imagens para a análise" />
         <SampleGallery.Grid imgArray={namedBlobs} />
-        <SampleGallery.Footer
-          closeBtnText="Fechar"
-          onConfirmAction={() => handleUpload()}
-        />
+        <SampleGallery.Footer closeBtnText="Fechar" onConfirmAction={() => handleUpload()} />
       </SampleGallery.Root>
 
       {fileStack.length !== 0 && (
@@ -204,7 +143,7 @@ export default function UploadFiles() {
           uploadCount={namedBlobs.length}
           src={namedBlobs}
           onCancelAction={() => setFileStack([])}
-          onConfirmAction={handleUpload}
+          onConfirmAction={() => handleUpload()}
         />
       )}
 
@@ -219,5 +158,5 @@ export default function UploadFiles() {
         </Result.Root>
       )}
     </>
-  )
+  );
 }
